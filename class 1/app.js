dccca0e0822e2db52afbbed1e427d0b339f798cb
SheikhAhmer <sheikhahmerali75@gmail.com>
2023-09-11 14:15:19 +0500

    Complete Classes Code

---
+++ class 1/app.js
@@ -0,0 +1,9 @@
+// alert("THANKS for comming today class");
+// alert("welcome on saturday");
+
+// keyword is for every declartion 
+// var let const ;
+
+var number = 764;
+var firstName = prompt("Enter ur first name please !");
+alert("welcome today Mr. "+firstName);
---
+++ class 2/app.js
@@ -0,0 +1,23 @@
+// var firstName1 = prompt("Enter your first name ?");
+
+
+// var firstNumber = 565 ;
+
+// var secondNumber =+ prompt("Enter second number");
+
+// var result = firstNumber + secondNumber ;
+
+// alert(result);
+
+//   deivde / multiply * add + subtract -
+
+
+var number = 1;
+// number++
+// number++
+console.log(number++);
+console.log(number);
+
+
+var numberCalculation = ((4 * (4 / (8 - 28))) + (49 + 17 ));
+console.log(numberCalculation);
